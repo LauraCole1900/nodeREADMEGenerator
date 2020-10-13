e5074a802aae1167de926ec182747f9f6c1ca98f
Laura Cole <lauracole1900@comcast.net>
2020-10-13 14:42:00 -0600

    inquirer

---
+++ index.js
@@ -0,0 +1,18 @@
+var inquirer = require("inquirer");
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
