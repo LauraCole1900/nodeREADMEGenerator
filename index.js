0c9d08e958ac7f02921742a4f97c0d131d8377fe
Laura Cole <lauracole1900@comcast.net>
2020-10-13 16:34:50 -0600

    questions

--- index.js
+++ index.js
@@ -1,7 +1,58 @@
 var inquirer = require("inquirer");
 
+// checklist of possible sections?
+// Title
+// TOC
+// Description
+// Installation
+// Usage
+// Technologies used
+// Problems faced
+// Contributors
+// License
+
 // array of questions for user
 const questions = [
+  { 
+    type: "",
+    message: "What is your project's title?",
+    name: "title"
+   },
+   {
+     type: "",
+     message: "What is your project's description?",
+     name: "description"
+   },
+   {
+     type: "",
+     message: "What are your project's installation instructions?",
+     name: "install"
+   },
+   {
+     type: "",
+     message: "WHat is your project's intended usage?",
+     name: "usage"
+   },
+   {
+     type: "",
+     message: "What technologies did your project use?",
+     name: "tech"
+   },
+   {
+     type: "",
+     message: "What problems did you face?",
+     name: "problems"
+   },
+   {
+    type: "",
+    message: "Who contributed to this project?",
+    name: "contributors"
+   },
+   {
+     type: "",
+     message: "How is this project licensed?",
+     name: "license"
+   }
 
 ];
 
