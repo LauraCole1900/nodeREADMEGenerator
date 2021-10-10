b90be86d6a920919a932c18a284382f73687adeb
Laura Cole <lauracole1900@comcast.net>
2021-10-10 11:17:06 -0600

    fixed spacing in index.js

--- index.js
+++ index.js
@@ -18,39 +18,39 @@ const path = require("path");
 
 // array of questions for user
 const questions = [
-  { 
+  {
     type: "input",
     message: "What is your project's title?",
     name: "title"
-   },
-   {
-     type: "input",
-     message: "What is your project's description?",
-     name: "description"
-   },
-   {
-     type: "input",
-     message: "What are your project's installation instructions?",
-     name: "install",
-     default: "npm i"
-   },
-   {
-     type: "input",
-     message: "What is your project's intended usage?",
-     name: "usage"
-   },
-   {
-     type: "input",
-     message: "What technologies did your project use?",
-     name: "tech"
-   },
-   {
+  },
+  {
+    type: "input",
+    message: "What is your project's description?",
+    name: "description"
+  },
+  {
+    type: "input",
+    message: "What are your project's installation instructions?",
+    name: "install",
+    default: "npm i"
+  },
+  {
+    type: "input",
+    message: "What is your project's intended usage?",
+    name: "usage"
+  },
+  {
+    type: "input",
+    message: "What technologies did your project use?",
+    name: "tech"
+  },
+  {
     type: "input",
     message: "What commands should be run to run tests?",
     name: "tests",
     default: "npm run test"
-   },
-   {
+  },
+  {
     type: "input",
     message: "Who needs credit on this project?",
     name: "credits"
@@ -70,12 +70,12 @@ const questions = [
     message: "What is your GitHub username?",
     name: "github"
   },
-   {
-     type: "list",
-     message: "How is this project licensed?",
-     name: "license",
-     choices: ["Apache", "Boost Software", "Creative Commons", "GNU", "ISC", "MIT", "Mozilla Public", "The Unlicense", "Other"]
-   }
+  {
+    type: "list",
+    message: "How is this project licensed?",
+    name: "license",
+    choices: ["Apache", "Boost Software", "Creative Commons", "GNU", "ISC", "MIT", "Mozilla Public", "The Unlicense", "Other"]
+  }
 ]
 
 // function to write README file
